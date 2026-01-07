01c5e692b22949cd33034d4144abb454f98810f2
Djamal-Kh <djamal.khizriev@mail.ru>
2026-01-07 13:36:38 +0300

    done with domen models

---
+++ DevQuestions/src/DevQuestions.Domain/Questions/Answer.cs
@@ -0,0 +1,9 @@
+﻿namespace DevQuestions.Domain.Questions;
+
+public class Answer
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public Question Question { get; set; }
+}
